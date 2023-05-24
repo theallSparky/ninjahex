8a8f1d1e1ae6bb9a60668452853a1f3e5e2b6baf
theallSparky <harrisonsparks.1999@gmail.com>
2023-05-24 01:06:51 -0500

    cleaned up code and appearance within canvas for the night

--- javascript.js
+++ javascript.js
@@ -1,29 +1,52 @@
 // A special thank you to youtuber 'Chris DeLeon of HomeTeam GameDev' 
 // with his video 'Coding an HTML5 Canvas Game with JS in 5 min 30 sec' 
-// for helping me with the setup            url = 
+// for helping me with the setup            
 window.onload = function() {
 
 
+    const canvas = document.getElementById('canvas');   // Generates the canvas upon loading up of the page
+    const c = canvas.getContext("2d");                  //Allows us to draw shapes onto the canvas
 
-    var canvas = document.getElementById('canvas'); // Generates the canvas upon loading up of the page
-    function resizeCanvas() {
-        //Some code to appropriately resize the canvas
-    }
-    var context = canvas.getContext("2d");      //Allows us to draw shapes onto the canvas
+    c.fillStyle = "black";                              // Change the fillstyle to black for player character
+    const playerSquare = c.rect(20, 20, 75, 75);        // Creates the shape of the player character
+    c.fill();                                           // Actually fills in the shape of player character
     
-    context.fillstyle = "#FF0000"
-    context.rect(50, 50, 75, 75);
-    context.fill();
 
-    function makeHex() {    //Draw and enclose Hex
-        context.moveTo(50, 50);
-        context.lineTo(50, 75);
-        context.stroke();
-        context.lineTo(75, 75);
-        context.stroke();
-        context.lineTo(75, 50);
-        context.stroke();
-        context.lineTo(50, 50);
-        context.stroke();
-    }
+    c.fillStyle = "green";                              // Change the fillstyle to green for start/end zones    
+    const startZone = c.strokeRect(10, 10, 100, 100);   // Creates the starting area
+    const endZone = c.strokeRect(650, 400, 100, 100);   // Creates the ending area
+
+
+
+
+    const bonusArea = c.strokeRect(625, 150, 40, 40);           // Where the bonus coin will be placed
+
+
+
+    // function makeHex() {    //Draw and enclose Hex
+    //     c.moveTo(50, 50);
+    //     c.lineTo(50, 75);
+    //     c.stroke();
+    //     c.lineTo(75, 75);
+    //     c.stroke();
+    //     c.lineTo(75, 50);
+    //     c.stroke();
+    //     c.lineTo(50, 50);
+    //     c.stroke();
+    // }
+
+
+    // function makeStartarea() {
+    //     c.moveto(1,1);
+    //     c.lineTo(1,101)
+    //     c.stroke();
+    //     c.lineTo(101,101)
+    //     c.stroke();
+    //     c.lineTo(101,1)
+    //     c.stroke();
+    //     c.lineTo(1,1)
+    //     c.stroke();
+    // }
+
+
 }
